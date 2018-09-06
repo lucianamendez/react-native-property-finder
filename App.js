b74ef0c94d2eb376715d4bb00dcf8f5ad9e961c5
Luciana Mendez <luciana.mendez@mediamonks.com>
2018-09-06 16:49:59 -0300

    add description to app.js

--- App.js
+++ App.js
@@ -5,45 +5,34 @@
  * @format
  * @flow
  */
+"use strict";
+import React, { Component } from "react";
+import { Platform, StyleSheet, Text, View } from "react-native";
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// const instructions = Platform.select({
+//   ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+//   android:
+//     "Double tap R on your keyboard to reload,\n" +
+//     "Shake or press menu button for dev menu"
+// });
 
 type Props = {};
+
 export default class App extends Component<Props> {
   render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+    return React.createElement(
+      Text,
+      { style: styles.description },
+      "Search for houses to buy!"
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  description: {
+    fontSize: 18,
+    textAlign: "center",
+    color: "#656565",
+    marginTop: 65
+  }
 });
